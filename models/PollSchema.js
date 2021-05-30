c44082ac4551fb11ae6cdcf7a36a8f68349ba462
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-30 09:38:59 +0800

    Create PollSchema.js

---
+++ models/PollSchema.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var PollSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    
+    question : {
+        type: String,
+        required: true
+    },
+
+    yesctr: {
+        type: [String],
+        required: false
+    },
+
+    noctr: {
+        type: [String],
+        required: false
+    }
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
