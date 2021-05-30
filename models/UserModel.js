fb040142ef5e54d291d30ceb874cc7f07f5be37e
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-30 11:19:03 +0800

    Create UserModel.js

---
+++ models/UserModel.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    fName: {
+        type: String,
+        required: true
+    },
+    lName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    polls: {
+        type: [String],
+        required: false
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
