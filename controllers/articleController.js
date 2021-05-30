1bd882c57f0d051a44184fae6110cf2d0c500a7b
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-31 04:01:50 +0800

    Update articleController.js

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,23 +1,80 @@
 const db = require('../models/db.js');
+const User = require('../models/UserModel.js');
 const Article = require('../models/ArticleModel.js');
 const Poll = require('../models/PollModel.js');
 
 const articleController = {
     getArticleList: function (req, res) {
         db.findMany(Article, {}, '', function(articles){
-            res.render('article-list', {articles : articles});
+            res.render('article-list', {articles : articles, username: req.session.username});
         });
     },
 
     getArticlePost: function (req, res) {
         var doc = {title: req.params.title}
-
+        var details = {}
         db.findOne(Article, doc, '', function(article){
             db.findOne(Poll, doc, '', function(poll){
-                res.render('article-post', {article:article, poll:poll});
+                details.article = article;
+                details.poll = poll;
+                details.username = req.session.username;
+
+                var total = poll.yesctr + poll.noctr;
+                var yespercent = poll.yesctr / total * 100;
+                var nopercent = poll.noctr / total * 100;
+                details.yespercent = yespercent.toFixed(2);;
+                details.nopercent = nopercent.toFixed(2);;
+                if (!req.session.username)
+                    details.notloggedin = 'Log in to answer the poll.';
+
+                db.findOne(User, {username: req.session.username}, '', function(user){
+                    if (user && user.polls.includes(req.params.title))
+                        details.answered = 'You have already answered this poll.';
+                    console.log(details)
+                    res.render('article-post', details);
+                });
             });
         });
-    }
+    },
+
+    incrementYesPoll: function (req, res) {
+        var doc = {title: req.query.title}
+        db.updateOne(User, {username: req.session.username}, {$push: {polls: req.query.title}}, function(result){
+            db.votePoll(Poll, doc, {'yesctr' : 1}, function(poll){
+                db.findOne(Article, doc, '', function(article){
+                    db.findOne(Poll, doc, '', function(poll){
+                        res.render('article-post', {article:article, poll:poll}, function(err, html){
+                            res.send(html);
+                        });
+                    });
+                });
+            });
+        });
+    },
+
+    incrementNoPoll: function (req, res) {
+        var doc = {title: req.query.title}
+        db.updateOne(User, {username: req.session.username}, {$push: {polls: req.query.title}}, function(result){
+            db.votePoll(Poll, doc, {'noctr' : 1}, function(poll){
+                db.findOne(Article, doc, '', function(article){
+                    db.findOne(Poll, doc, '', function(poll){
+                        res.render('article-post', {article:article, poll:poll}, function(err, html){
+                            res.send(html);
+                        });
+                    });
+                });
+            });
+        });
+    },
+
+    checkPollAnswered: function(req, res) {
+        var doc = {title: req.query.title}
+        db.findOne(User, {username: req.session.username}, '', function (user) {
+            if (user && user.polls.includes(req.query.title))
+                res.send(true);
+            else res.send(false);
+        });
+    },
 }
 
 module.exports = articleController;
