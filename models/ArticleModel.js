fce142a591ffb7cc68e8b2893986928a3cbaff59
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-30 09:38:53 +0800

    Create ArticleModel.js

---
+++ models/ArticleModel.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+
+var ArticleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    
+    author : {
+        type: String,
+        required: true
+    },
+
+    content: {
+        type: [String],
+        required: false
+    },
+
+    references: {
+        type: [String],
+        required: false
+    },
+
+    imgpath: {
+        type: String,
+        required: false
+    },
+
+    imgsrc: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
