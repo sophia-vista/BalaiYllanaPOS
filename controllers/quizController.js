305954443ae76e63e16bf124c30ea5d0c35c369a
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-31 14:15:31 +0800

    Update quizController.js

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,6 +1,23 @@
+const db = require('../models/db.js');
+const Quiz = require('../models/QuizModel.js');
+
 const quizController = {
     getQuizList: function (req, res) {
-        res.render('quiz-list');
+        db.findMany(Quiz, {}, '', function(quizzes){
+            res.render('quiz-list', {quizzes : quizzes, username: req.session.username});
+        });
+    },
+
+    getQuizPost: function (req, res) {
+        var doc = {title: req.params.title}
+        var details = {}
+
+        db.findOne(Quiz, doc, '', function(quiz){
+            details.quiz = quiz;
+            details.username = req.session.username;
+
+            res.render('quiz-post', details);
+        });
     }
 }
 
